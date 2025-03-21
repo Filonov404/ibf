2d6b23273a3950d7c13d5824ef60372e309687e0
aleksandrfilonov <fffilonov31@gmail.com>
2025-03-21 11:47:22 +0300

    Начал слайдер

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 // function checkWidth() {
 //     var windowWidth = $(window).width();
-    
+
 //     if (windowWidth < 768) {
 //         $("#products-gallery").addClass("mt-40");
 //     } else {
@@ -14,28 +14,45 @@
 // $(window).resize(function() {
 //     checkWidth();
 // });
+document.addEventListener('DOMContentLoaded', () => {
+
+    const burger = document.querySelector('.burger');
+    const burgerSpan = document.querySelector('.burger span');
+    const menu = document.querySelector('.menu');
+    const menuItems = document.querySelectorAll('.menu li');
+
+    burger.addEventListener('click', () => {
+        document.body.classList.toggle("no-scroll");
+        menu.classList.toggle('open');
+        burgerSpan.classList.toggle('open-burger');
+    });
 
 
-const burger = document.querySelector('.burger');
-const burgerSpan = document.querySelector('.burger span');
-const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menu li');
+    document.addEventListener('click', (event) => {
+        if (!menu.contains(event.target) && !burger.contains(event.target)) {
+            menu.classList.remove('open');
+            document.body.classList.remove("no-scroll");
+            burgerSpan.classList.remove('open-burger');
+            menuItems.forEach(item => {
+                item.classList.remove('open');
+            });
+        }
+    });
 
-burger.addEventListener('click', () => {
-    document.body.classList.toggle("no-scroll");
-    menu.classList.toggle('open');
-    burgerSpan.classList.toggle('open-burger');
-});
 
 
-document.addEventListener('click', (event) => {
-    if (!menu.contains(event.target) && !burger.contains(event.target)) {
-        menu.classList.remove('open');
-        document.body.classList.remove("no-scroll");
-        burgerSpan.classList.remove('open-burger');
-        menuItems.forEach(item => {
-            item.classList.remove('open');
-        });
+
+    const paragraph = document.getElementById('big-first-word');
+    const text = paragraph.textContent;
+    const words = text.trim().split(' ');
+
+    if (words.length > 0) {
+        const firstWord = words[0];
+        const restOfText = words.slice(1).join(' ');
+        paragraph.innerHTML = `<span class="first-word">${firstWord}</span> ${restOfText ? ' ' + restOfText : ''}`;
     }
-});
 
+
+    top-slider-init-wrap
+
+});
